f814f7e7f986735c7c4b64e3421ae13dc32a7ccc
admin <admin@admins-MacBook-Pro.local>
2020-02-01 23:12:19 -0800

    added controller and routers for database.

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
-const bookRoutes = require("./books");
+const ListsRoutes = require("./Lists");
+const ItemRoutes = require("./Item");
+const ListRoutes = require("./List");
 
 // Book routes
-router.use("/books", bookRoutes);
+router.use("/Lists", ListsRoutes);
+router.use("/List", ListRoutes);
+router.use("/Item", ItemRoutes);
 
 module.exports = router;
