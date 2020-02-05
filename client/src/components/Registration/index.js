3689e7e1bc85b882d84006b4ba3430fb3ebd49cd
Ray He <rayhe921@gmail.com>
2020-02-04 19:50:57 -0800

    update login page

--- client/src/components/Registration/index.js
+++ client/src/components/Registration/index.js
@@ -3,52 +3,28 @@ import "./style.css";
 
 function Registration(prop) {
     return (
-        <form>
-            <div class="form-row">
-                <div class="form-group col-md-6">
-                    <label for="inputEmail4">Email</label>
-                    <input type="email" class="form-control" id="inputEmail4" />
+        <div className="col-6 card">
+            <h2 className="text-center">Sign Up Here</h2>
+            <h5 className="text-center">It's quick and easy</h5>
+            <hr />
+            <form>
+                <div>
+                    <div className="form-group col-md-6">
+                        <label for="username">Username</label>
+                        <input type="text" placeholder="New Username Here" className="form-control" id="username" />
+                    </div>
+                    <div className="form-group col-md-6">
+                        <label for="inputPassword4">Password</label>
+                        <input type="password" placeholder=" New Password Here" className="form-control" id="inputPassword4" />
+                    </div>
                 </div>
-                <div class="form-group col-md-6">
-                    <label for="inputPassword4">Password</label>
-                    <input type="password" class="form-control" id="inputPassword4" />
+                <div className="form-group col-md-6">
+                    <label for="inputEmail">E-Mail Address</label>
+                    <input type="email" className="form-control" id="inputEmail" placeholder="email@email.com" />
                 </div>
-            </div>
-            <div class="form-group">
-                <label for="inputAddress">Address</label>
-                <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St" />
-            </div>
-            <div class="form-group">
-                <label for="inputAddress2">Address 2</label>
-                <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor" />
-            </div>
-            <div class="form-row">
-                <div class="form-group col-md-6">
-                    <label for="inputCity">City</label>
-                    <input type="text" class="form-control" id="inputCity" />
-                </div>
-                <div class="form-group col-md-4">
-                    <label for="inputState">State</label>
-                    <select id="inputState" class="form-control">
-                        <option selected>Choose...</option>
-                        <option>...</option>
-                    </select>
-                </div>
-                <div class="form-group col-md-2">
-                    <label for="inputZip">Zip</label>
-                    <input type="text" class="form-control" id="inputZip" />
-                </div>
-            </div>
-            <div class="form-group">
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" id="gridCheck" />
-                    <label class="form-check-label" for="gridCheck">
-                        Check me out
-            </label>
-                </div>
-            </div>
-            <button type="submit" class="btn btn-primary">Sign in</button>
-        </form>
+                <button type="submit" class="btn btn-primary col align-self-center">Sign in</button>
+            </form>
+        </div>
     )
 }
 
