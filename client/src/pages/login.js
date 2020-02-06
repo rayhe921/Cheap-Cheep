daf12bafd98c574ca55523063c90b9d3211af60a
Ray He <rayhe921@gmail.com>
2020-02-06 12:04:27 -0800

    added infocard component to login page

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,14 +1,19 @@
 import React, { Component } from "react";
 import Registration from "../components/Registration";
+import InfoCard from "../components/InfoCard";
 
 
 class Login extends Component {
   render() {
     return (
-      
-      <Registration>
-
-      </Registration>
+      <div>
+      <InfoCard
+        listOne="Easy"
+        listTwo="Organized"
+        listThree="Cheap on your budget"
+      ></InfoCard>
+      <Registration></Registration>
+      </div>
     );
   }
 }
