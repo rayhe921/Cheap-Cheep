6f0dbb2f88c46513a4901262f9f54a16830a87d1
e-holmes <bethholmes768@gmail.com>
2020-02-07 00:46:57 -0800

    basic side bar and fixed input

--- client/src/components/ShopList/index.js
+++ client/src/components/ShopList/index.js
@@ -1,36 +1,31 @@
 import React from "react";
 import "./style.css";
 
-function ShopList(prop) {
+function ShopList(props) {
     return (
-        <div className="col-9 justify-content-center">
-        <div className="col-8 text-center d-flex justify-content-between">
-            <table className="table table-bordered table-hover table-sm m-7">
-                <thead>
-                    <tr>
-                        <th>Remove</th>
-                        <th>Item Name</th>
-                        <th>Lowest Price</th>
-                        <th>Link</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th className="">
-                            <button type="button" class=" btn-sm btn btn-outline-danger btn-dark">X</button>
-                        </th>
-                        <td>Toothbrush</td>
-                        <td>$ 5.99</td>
-                        <td>@Amazon</td>
-                    </tr>
-                </tbody>
-            </table>
+        <div>
+            <div className="col-8 text-center d-flex justify-content-between">
+                <table className="table table-bordered table-hover table-sm m-7">
+                    <thead>
+                        <tr>
+                            <th>Remove</th>
+                            <th>Item Name</th>
+                            <th>Lowest Price</th>
+                            <th>Link</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th className="">
+                                <button type="button" class=" btn-sm btn btn-outline-danger btn-dark">X</button>
+                            </th>
+                            <td>Toothbrush</td>
+                            <td>$ 5.99</td>
+                            <td>@Amazon</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
-            <div className="col-8 text-center d-flex justify-content-center">
-            <form>
-                <button type="submit" class="btn btn-primary col align-self-center">Sign in</button>
-            </form>
-        </div>
         </div>
     )
 }
--- client/src/pages/display.js
+++ client/src/pages/display.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
-import Compile from "./compile";
+import Sidebar from "../components/Sidebar"
+import Shoplist from "../components/ShopList";
+import Input from "../components/Input"
 
 class Display extends Component {
   render() {
     return (
-      <Compile>
-
-      </Compile>
+      <div className="row">
+        <div className="col-3">
+        <Sidebar></Sidebar>
+        </div>
+        <div className="col-9">
+        <Shoplist></Shoplist>
+        <Input></Input>
+        </div>
+      </div>
     );
   }
 }
