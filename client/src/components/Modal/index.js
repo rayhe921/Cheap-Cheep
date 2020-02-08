ee3ee89029f542cf06c919a959e5ddafbdc3bf72
e-holmes <bethholmes768@gmail.com>
2020-02-08 11:07:49 -0800

    X button works now

--- client/src/components/Modal/index.js
+++ client/src/components/Modal/index.js
@@ -11,7 +11,7 @@ function Modal(prop) {
                 <div className="modal-content">
                     <div className="modal-header">
                         <h5 className="modal-title">{prop.title}</h5>
-                        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                        <button type="button" className="close" onClick={prop.hideModal} data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
                     </div>
