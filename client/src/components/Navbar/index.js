005796d4e236ed1cd35f33f13baebf5dec969098
eawhite00 <eawhite00@gmail.com>
2020-02-11 19:16:00 -0800

    to exist is to suffer

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,91 +1,47 @@
-import React, { Component } from "react";
+import React from "react";
 import "./style.css";
-import API from "../../utils/API";
 
-class Navbar extends Component {
-
-  state = {
-    loginName: "",
-    loginPass: "",
-};
-
-handleInputChange = event => {
-  // Getting the value and name of the input which triggered the change
-  const { name, value } = event.target;
-
-  // Updating the input's state
-  this.setState({
-      [name]: value
-  });
-
-  // console.log(this.state);
-};
-
-handleFormSubmit = event => {
-  // Preventing the default behavior of the form submit (which is to refresh the page)
-  event.preventDefault();
-
-  if (this.state.loginName && this.state.loginPass) {
-      API.login({
-          userName: this.state.loginName,
-          password: this.state.loginPass,
-
-      }).then( function(response) {
-        console.log(response);
-
-        if (response.data.found === false){
-          console.log("User not found.")
-        } else if (response.data.userID === -1){
-          console.log("Password is incorrect");
-        } else {
-           localStorage.setItem("id", response.data.userID);
-           window.location.href = "/main";
-        }
-      })
-          .catch(err => console.log(err));  
-
-      // Alert the user their first and last name, clear `this.state.firstName` and `this.state.lastName`, clearing the inputs
-  }
-};
-
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="/">
-          <img src="img" width="100" height="100" alt="logo" />
-        </a>
-        <span className="navbar-text text-center">
-          <h1>
-            {/* {props.title} */}
-            Title
-          </h1>
+function Navbar(props) {
+  const showLogout = props.showLogin ? "form-group" : "form-group display-none";
+  const hideLogoutButton = props.hideLogoutButton ? "display-none" : "display-block";
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="/">
+        <img src={props.image} width="100" height="100" alt="logo" />
+      </a>
+      <span className="navbar-text text-center">
+        <h1>
+          {props.title}
+        </h1>``
         </span>
-        <div className="form-group">
-          <form className="form-inline">
-            <input 
-            name="loginName" 
-            value={this.state.loginName}
-            onChange={this.handleInputChange}
-            className="form-control mr-sm-2" 
-            type="username" 
-            placeholder="Username" 
+      <div className={showLogout}>
+        <form className="form-inline">
+          <input
+            name="loginName"
+            value={props.loginNameValue}
+            onChange={props.handleInputChange}
+            className="form-control mr-sm-2"
+            type="username"
+            placeholder="Username"
             aria-label="Username" />
-            <input 
-            name="loginPass" 
-            value={this.state.loginPass}
-            onChange={this.handleInputChange}
-            className="form-control mr-sm-2" 
-            type="password" 
-            placeholder="Password" 
+          <input
+            name="loginPass"
+            value={props.passwordValue}
+            onChange={props.handleInputChange}
+            className="form-control mr-sm-2"
+            type="password"
+            placeholder="Password"
             aria-label="Password" />
-          </form>
-          <div>
-            <button id="login-button" onClick={this.handleFormSubmit} className="btn btn-outline-success my-2" type="submit">login</button>
-          </div>
+        </form>
+        <div>
+          <button id="login-button" onClick={props.onClick} className="btn btn-outline-success my-2" type="submit">login</button>
         </div>
-      </nav>
-    );
-  }
+      </div>
+      <div className={hideLogoutButton}>
+        <button onClick={props.onClickLogout} className="btn btn-outline-success my-2" type="submit">{props.buttonNameTwo}</button>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
