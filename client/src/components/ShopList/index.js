41f7aa2d9dbb723686b199bac166c084139bf2c9
Ray He <rayhe921@gmail.com>
2020-02-11 21:17:52 -0800

    merge

--- client/src/components/ShopList/index.js
+++ client/src/components/ShopList/index.js
@@ -1,32 +1,32 @@
 import React, { Component } from "react";
 import API from "../../utils/API";
 import "./style.css";
-import List from "../List";
+// import List from "../List";
 
 
 
 
 class ShopList extends Component {
-  state = {
-    Items: []
-  };
+    state = {
+        Items: []
+    };
 
-  componentDidMount() {
-    this.loaditem();
-  }
+    componentDidMount() {
+        this.loaditem();
+    }
 
-  loaditem = () => {
-    API.getItem()
-      .then(res => this.setState({ Items: res.data }))
+    loaditem = () => {
+        API.getItem()
+            .then(res => this.setState({ Items: res.data }))
 
-      .catch(err => console.log(err));
+            .catch(err => console.log(err));
 
-  };
+    };
 
 
-  render() {
-    console.log(this.state.Items);
-    return (
+    render() {
+        console.log(this.state.Items);
+        return (
             <div className="col-8 text-center d-flex justify-content-between">
                 <table className="table table-bordered table-hover table-sm m-7">
                     <thead>
@@ -35,31 +35,31 @@ class ShopList extends Component {
                             <th>Item Name</th>
                             <th>Lowest Price</th>
                             <th>Link</th>
-              <th>search Term</th>
+                            <th>search Term</th>
 
-            </tr>
-          </thead>
-          <tbody>
-            <tr Items={this.state.Item} >
-              {this.state.Items.map(Item => (
-                <tbody key={Item._id}>
-                  <tr>
-                    <th className="">
-                      <button type="button" className=" btn-sm btn btn-outline-danger btn-dark">X</button>
-                    </th>
-                    <td>{Item.name}</td>
-                    <td>{Item.price}</td>
-                    <td>{Item.website}</td>
-                    <td>{Item.seacrhTerm}</td>
-                  </tr>
-                </tbody>
-              ))}
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    );
-  }
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr Items={this.state.Item} >
+                            {this.state.Items.map(Item => (
+                                <tbody key={Item._id}>
+                                    <tr>
+                                        <th className="">
+                                            <button type="button" className=" btn-sm btn btn-outline-danger btn-dark">X</button>
+                                        </th>
+                                        <td>{Item.name}</td>
+                                        <td>{Item.price}</td>
+                                        <td>{Item.website}</td>
+                                        <td>{Item.seacrhTerm}</td>
+                                    </tr>
+                                </tbody>
+                            ))}
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
 }
 
 export default ShopList;
